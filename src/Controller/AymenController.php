7e56065c80b3635ae873ce45371fb48a18bef908
Aymen boumliki <aymenboumliki2007@gmail.com>
2026-01-06 01:04:34 +0100

    Premier envoi : Portfolio avec attestation SecNum

--- src/Controller/AymenController.php
+++ src/Controller/AymenController.php
@@ -4,15 +4,52 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class AymenController extends AbstractController
+/**
+ * Contrôleur principal du Portfolio - Aymen Boumliki
+ */
+class AymenController extends AbstractController
 {
-    #[Route('/aymen', name: 'app_aymen')]
+    /**
+     * Page d'accueil : Présentation générale
+     * Route : /
+     */
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('aymen/index.html.twig', [
-            'controller_name' => 'AymenController',
-        ]);
+        return $this->render('portfolio/index.html.twig');
+    }
+
+    /**
+     * Page Parcours & Compétences
+     * Route : /mon-parcours
+     */
+    #[Route('/mon-parcours', name: 'app_cv')]
+    public function cv(): Response
+    {
+        return $this->render('portfolio/cv.html.twig');
+    }
+
+    /**
+     * Page Loisirs (Judo, Coaching, Création Web)
+     * Route : /mes-loisirs
+     */
+    #[Route('/mes-loisirs', name: 'app_projects')]
+    public function projects(): Response
+    {
+        // IMPORTANT : Vérifie que ton fichier s'appelle bien 'projet.html.twig' (sans S)
+        // dans le dossier templates/portfolio/
+        return $this->render('portfolio/projet.html.twig');
+    }
+
+    /**
+     * Page Questionnaire d'avis pour les visiteurs
+     * Route : /donner-mon-avis
+     */
+    #[Route('/donner-mon-avis', name: 'app_avis')]
+    public function avis(): Response
+    {
+        return $this->render('portfolio/avis.html.twig');
     }
 }
